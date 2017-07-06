d3a4a9449701661c9010fc9c9efe6bc605c12740
bums <s7515199@gmail.com>
2017-07-06 09:55:22 +0000

    adding javafile in master

---
+++ Hello.java
@@ -0,0 +1,8 @@
+public class Hello{
+	public static void main(String[] args){
+		float f=1.23f;//float 형식에는 접미에 f를 붙여줘야 함.
+		double d=1.23;
+		System.out.println("the value of 'f'is "+f);
+	
+	}
+}
