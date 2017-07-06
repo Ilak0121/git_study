5ef807f7b30ab29903f42ca60d72a2b1525235e6
bums <s7515199@gmail.com>
2017-07-06 09:58:46 +0000

    adding master upddate

--- Hello.java
+++ Hello.java
@@ -3,6 +3,6 @@ public class Hello{
 		float f=1.23f;//float 형식에는 접미에 f를 붙여줘야 함.
 		double d=1.23;
 		System.out.println("the value of 'f'is "+f);
-	
+		System.out.println("this is for master branch update");
 	}
 }
