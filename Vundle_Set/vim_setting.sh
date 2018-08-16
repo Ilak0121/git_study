dae3b19dd666a7efb2b31610f0cc408dbfa16b79
ilak0121 <s7515199@gmail.com>
2018-08-17 04:44:29 +0900

    git study arrange

--- Vundle_Set/vim_setting.sh
+++
@@ -1,5 +0,0 @@
-#To setting vim_vundle shell code
-git clone https://github.com/VundleVim/Vundle.vim ~/.vim/bundle/Vundle.vim
-mv vim_set.data ~/.vimrc
-vim +PluginInstall +qall
-
--- archive_change.sh
+++
@@ -1,3 +0,0 @@
-sudo cp /etc/apt/source.list /etc/apt/source.list.bak
-sudo sed 's/kr.archive.ubuntu.com/ftp.daumkakao.com/' -i  /etc/apt/sources.list
-#/etc/apt/source.list가 우리가 리눅스를 통해 install or upgrade할때 받아오는 서번데 현재 한국서버가 원할하지 않은 관계로 kakao 서버로 대체 하기 위해 툴을 썼습니다. 위 내용이 어려운 건 아니니 찾아보면 무슨 의민지 금방 이해할듯 ㅇㅇ
